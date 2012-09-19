1df36aee9b067a7e7017dbe51132d76ac26f4de4
RyumaN <philosophers.windurst@gmail.com>
2012-09-20 06:08:08 +0900

    再プッシュ

---
+++ src/daily0918/FusenGameMain.java
@@ -0,0 +1,82 @@
+package daily0918;
+
+import java.awt.Graphics;
+
+import daily0917.GameHoneGumi;
+
+public class FusenGameMain extends GameHoneGumi {
+
+	public FusenGameMain(int w, int h, String title) {
+		super(w, h, title);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@Override
+	public void initStageStart() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void initStageClear() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void initGameOver() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyPressedGameMain(int keycode) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void keyReleasedGameMain(int keycode) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void runStartGamen(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void runStageStart(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void runStageClear(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void runGameMain(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void runGameOver(Graphics g) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
