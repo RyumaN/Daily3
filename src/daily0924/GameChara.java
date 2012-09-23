06c6381af9f35a9e914520b5c1de4c1790067321
RyumaN <philosophers.windurst@gmail.com>
2012-09-24 04:53:58 +0900

    0924

---
+++ src/daily0924/GameChara.java
@@ -0,0 +1,31 @@
+package daily0924;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+public abstract class GameChara {
+	public int chara_x, chara_y;
+	int image_x, image_y, image_w, image_h;
+	BufferedImage image1;
+
+	GameChara(int x, int y, BufferedImage img, int ix, int iy, int iw, int ih){
+		chara_x = x;
+		chara_y = y;
+
+		image1 = img;
+
+		image_x = ix;
+		image_y = iy;
+		image_w = iw;
+		image_h = ih;
+	}
+	
+	public void draw(Graphics g, ImageObserver io){
+		g.drawImage(image1, chara_x, chara_y, chara_x + image_w, chara_y + image_h, 
+				image_x, image_y, image_x + image_w, image_y + image_h, io);
+		move();
+	}
+	
+	public abstract void move();
+}
---
+++ src/daily0924/Jiki.java
@@ -0,0 +1,24 @@
+package daily0924;
+
+import java.awt.image.BufferedImage;
+
+public class Jiki extends GameChara {
+
+	public Jiki(int x, int y, BufferedImage img) {
+		super(x, y, img, 0, 0, 48, 48);
+
+	}
+
+	@Override
+	public void move() {
+
+
+	}
+
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
