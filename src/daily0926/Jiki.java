1d32f0fd3923a42c04d3e5cf9a6af74c050caf10
RyumaN <philosophers.windurst@gmail.com>
2012-09-30 14:32:45 +0900

    0926

---
+++ src/daily0926/Jiki.java
@@ -0,0 +1,21 @@
+package daily0926;
+
+import java.awt.image.BufferedImage;
+
+public class Jiki extends GameChara {
+
+	public Jiki(int x, int y, BufferedImage img) {
+		super(x, y, 12, 12, img, 0, 0, 48, 48);
+
+	}
+
+	@Override
+	public void move() {
+
+
+	}
+
+
+
+
+}
