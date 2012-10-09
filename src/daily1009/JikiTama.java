bc1617eef1a21d028e9c74d6fc8b9d88579eb224
RyumaN <philosophers.windurst@gmail.com>
2012-10-09 15:36:35 +0900

    1009爆発クラス作成

---
+++ src/daily1009/JikiTama.java
@@ -0,0 +1,18 @@
+package daily1009;
+
+import java.awt.image.BufferedImage;
+
+public class JikiTama extends GameChara {
+
+	public JikiTama(int x, int y, BufferedImage img) {
+		super(x, y, 12, 12, img, 240, 0, 12, 12);
+
+	}
+
+	@Override
+	public void move() {
+		chara_x = chara_x + 12;
+
+	}
+
+}
