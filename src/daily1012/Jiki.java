91758f053e057e4edf3d1247ab5341bb425d2661
RyumaN <philosophers.windurst@gmail.com>
2012-10-13 05:11:42 +0900

    1012ステージ開始、ステージクリア、ゲームオーバーのソース

---
+++ src/daily1012/Jiki.java
@@ -0,0 +1,32 @@
+package daily1012;
+
+import java.awt.image.BufferedImage;
+
+public class Jiki extends GameChara {
+
+	public Jiki(int x, int y, BufferedImage img) {
+		super(x, y, 12, 12, img, 0, 0, 48, 48);
+
+	}
+
+	@Override
+	public void move() {
+
+	}
+
+	public void move(boolean up, boolean down, boolean left, boolean right){
+		if (up == true) chara_y = chara_y - 8;
+		if (down == true) chara_y = chara_y + 8;
+		if (left == true) chara_x = chara_x - 8;
+		if (right == true) chara_x = chara_x + 8;
+
+		if (chara_y < 0) chara_y = 0;
+		if (chara_x < 0) chara_x = 0;
+		if (chara_y >UtyuuShoot.GAMEN_H - 48)
+				chara_y = UtyuuShoot.GAMEN_H - 48;
+		if (chara_x > UtyuuShoot.GAMEN_W - 48)
+				chara_x = UtyuuShoot.GAMEN_W - 48;
+	}
+
+
+}
---
+++ src/daily1012/PatternReader.java
@@ -0,0 +1,35 @@
+package daily1012;
+
+import java.awt.Point;
+import java.util.StringTokenizer;
+
+public class PatternReader {
+	String patstr;
+	StringTokenizer tokenizer;
+	Point movexy = new Point();
+	int kaisuu = 0;
+
+
+
+
+	PatternReader(String str){
+		patstr = str;
+		tokenizer = new StringTokenizer(patstr, ",");
+	}
+
+	Point next(){
+		if (kaisuu == 0){
+			if (tokenizer.hasMoreTokens() == false){
+					tokenizer = new StringTokenizer(patstr, ",");
+			}
+			movexy.x = Integer.parseInt(tokenizer.nextToken());
+			movexy.y = Integer.parseInt(tokenizer.nextToken());
+			kaisuu = Integer.parseInt(tokenizer.nextToken());
+		} else {
+			kaisuu = kaisuu - 1;
+		}
+		return movexy;
+
+	}
+
+}
