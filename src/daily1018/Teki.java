3d5a5bbe46ef14b008688dbcb3cec71eb191789d
RyumaN <philosophers.windurst@gmail.com>
2012-10-20 04:29:04 +0900

    1018横シューティング

---
+++ src/daily1018/Teki.java
@@ -0,0 +1,28 @@
+package daily1018;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+
+public class Teki extends GameChara {
+	PatternReader preader;
+	int hanten;
+
+	public Teki(int x, int y, BufferedImage img, int ptnum, String patstr) {
+		super(x, y, 40, 40, img, ptnum * 48, 48, 48, 48);
+
+		preader = new PatternReader(patstr);
+		if (chara_y > YokoShootMain.GAMEN_H / 2) {
+			hanten = - 1;
+		} else {
+			hanten = 1;
+		}
+
+	}
+
+	public void move() {
+		Point movexy = preader.next();
+		chara_x = chara_x + movexy.x;
+		chara_y = chara_y + movexy.y * hanten;
+	}
+}
