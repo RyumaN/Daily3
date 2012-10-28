946cdde04d609c15dcb44c699b4a6b5570f21ced
RyumaN <philosophers.windurst@gmail.com>
2012-10-29 05:47:43 +0900

    1026

---
+++ src/daily1026/Sample.java
@@ -0,0 +1,25 @@
+package daily1026;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Sample extends Applet {
+	Image img;
+
+//画像表示
+/*	public void paint(Graphics g){
+		this.img = getImage(hoge, "hoge.jpg");
+		g.drawImage(hoge, 10, 10, this);
+
+		this.img = getImage(hoge, "hoge2.jpg");
+		g.drawImage(hoge, 10, 10, this);
+	}*/
+
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
