aa6174c4f8a9a6e821da3e19acdfc22653b5831f
RyumaN <philosophers.windurst@gmail.com>
2012-11-12 07:07:06 +0900

    1112 JFrame

---
+++ src/daily1112/Sample.java
@@ -0,0 +1,35 @@
+package daily1112;
+
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Checkbox;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Label;
+import java.awt.MediaTracker;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+
+public class Sample extends Applet {
+
+
+	public static void main(String[] args) {
+		JFrame jf = new JFrame();
+		jf.setSize(200, 100);
+		jf.setDefaultCloseOperation(jf.EXIT_ON_CLOSE);
+		jf.setTitle("JFrame");
+		jf.setVisible(true);
+
+
+	}
+
+}
