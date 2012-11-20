27686d10c72cc62fb984e94e86e82c32dd288c17
RyumaN <philosophers.windurst@gmail.com>
2012-11-20 20:46:40 +0900

    1119 while

---
+++ src/daily1119/Sample.java
@@ -0,0 +1,22 @@
+package daily1119;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			char carray[] = {'あ', 'あ', 'あ', 'え', 'あ'};
+
+			int i = 0;
+
+			while (carray[i] != 'え'){
+				System.out.println(carray[i]);
+				i ++;
+			}
+
+	}
+
+}
