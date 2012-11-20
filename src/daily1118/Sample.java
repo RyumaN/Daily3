6669e5cff50e2f596dd62e1d53a7b404df8a429e
RyumaN <philosophers.windurst@gmail.com>
2012-11-20 20:40:15 +0900

    1118 for

---
+++ src/daily1118/Sample.java
@@ -0,0 +1,18 @@
+package daily1118;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			char carray[] = {'あ', 'い', 'う', 'え', 'お'};
+
+			for (int i = 0; i < 5; i ++){
+				System.out.println("carray[" + i + "] → " + carray[i]);
+			}
+	}
+
+}
