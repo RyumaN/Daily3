98986d78a323f31b0040dfbd9e478eff333e1fe6
RyumaN <philosophers.windurst@gmail.com>
2012-11-28 20:00:17 +0900

    1122 constructor

---
+++ src/daily1122/Sample.java
@@ -0,0 +1,26 @@
+package daily1122;
+
+import java.awt.Container;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			Date now;
+			String nichiji;
+
+			now = new Date();
+
+			nichiji = now.toString();
+
+			System.out.println(nichiji);
+			}
+
+
+	}
+
+
