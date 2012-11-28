37ef2cee525e9d84aedee0d544675fe798b9f4ed
RyumaN <philosophers.windurst@gmail.com>
2012-11-28 21:14:06 +0900

    1128 Math.min

---
+++ src/daily1128/Sample.java
@@ -0,0 +1,21 @@
+package daily1128;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			int i1 = -100, i2 = 99, i3;
+
+			i3 = Math.min(i1, i2);
+
+			System.out.println(i1 + "と" + i2 + ", 小さい方は" + i3);
+			}
+
+
+	}
+
+
