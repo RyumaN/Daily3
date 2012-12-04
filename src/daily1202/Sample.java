36eb89e853428d9e52ca6db89256afb53508f421
RyumaN <philosophers.windurst@gmail.com>
2012-12-04 15:31:40 +0900

    1202 Math.random 乱数

---
+++ src/daily1202/Sample.java
@@ -0,0 +1,21 @@
+package daily1202;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			double d1;
+
+			d1 = Math.random() * 10;
+
+			System.out.println("今回の乱数は" + d1);
+			}
+
+
+	}
+
+
