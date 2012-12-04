452700c50186a43c2562fe63eda3881c48b194c3
RyumaN <philosophers.windurst@gmail.com>
2012-12-04 15:25:26 +0900

    1201 Math.pow 累乗

---
+++ src/daily1201/Sample.java
@@ -0,0 +1,25 @@
+package daily1201;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			double d1, d2, d3;
+
+			d1 = Math.pow(2, 8);
+			d2 = Math.pow(2, 16);
+			d3 = Math.pow(2, 24);
+
+			System.out.println("8bitカラーは" + d1 + "色");
+			System.out.println("16bitカラーは" + d2 + "色");
+			System.out.println("24bitカラーは" + d3 + "色");
+			}
+
+
+	}
+
+
