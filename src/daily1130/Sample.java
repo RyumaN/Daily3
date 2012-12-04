b729e31c0bf6fcc6a59da77ad1f50fa93f8f3f44
RyumaN <philosophers.windurst@gmail.com>
2012-12-04 15:19:30 +0900

    1130 Math,sqrt 平方根

---
+++ src/daily1130/Sample.java
@@ -0,0 +1,21 @@
+package daily1130;
+	import java.awt.Container;
+
+	import javax.swing.JFrame;
+	import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			double d1 = 256, d2;
+
+			d2 = Math.sqrt(d1);
+
+			System.out.println(d1 + "の平方根は" + d2);
+			}
+
+
+	}
+
+
