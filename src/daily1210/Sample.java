3becaf6855cb170555c5102565aa1e7c91fe10d0
RyumaN <philosophers.windurst@gmail.com>
2012-12-10 20:56:55 +0900

    1210 String 文字列検索

---
+++ src/daily1210/Sample.java
@@ -0,0 +1,29 @@
+package daily1210;
+	import java.awt.Container;
+import java.util.Calendar;
+import java.util.Date;
+
+	import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+		public static void main(String[] args) {
+			String str = "Javaパーフェクトマスター";
+
+			System.out.println(str + "の");
+
+			System.out.println("4番目の文字は「" + str.charAt(4) + "」です。");
+
+			System.out.println("10番目の文字は「" + str.charAt(10) + "」です。");
+
+			System.out.println("4番目以降の文字は「" + str.substring(4) + "」です。");
+
+			System.out.println("10番目以降の文字は「" + str.substring(10) + "」です。");
+			}
+
+
+	}
+
+
