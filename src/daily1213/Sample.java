78101d4fd467b5f7a82fd38c4ec84a214fbd5741
RyumaN <philosophers.windurst@gmail.com>
2012-12-14 23:40:18 +0900

    1213 String 部分文字列を置換

---
+++ src/daily1213/Sample.java
@@ -0,0 +1,28 @@
+package daily1213;
+	import java.awt.Container;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+	import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+
+			public static void  main(String[] args){
+				String str_old = "JavaパーフェクトマスターJava";
+
+				String str_new = str_old.replaceAll("Java", "JAVA");
+
+				System.out.println(str_new);
+
+
+
+
+		}
+
+	}
+
+
