1a17e52ecd646fee2362ab963742a495bf33aef0
RyumaN <philosophers.windurst@gmail.com>
2012-12-14 23:44:51 +0900

    1214 文字列の長さを調べる

---
+++ src/daily1214/Sample.java
@@ -0,0 +1,29 @@
+package daily1214;
+	import java.awt.Container;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+	import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+
+	public class Sample {
+
+
+
+			public static void  main(String[] args){
+				String str = "JavaパーフェクトマスターA１";
+
+				int len = str.length();
+
+				System.out.println(str);
+				System.out.println("は" + len + "文字です");
+
+
+
+
+		}
+
+	}
+
+
